d52c407fe5c59d28401f9bc2d9b8b8a88e807f20
Brock Wilson <brock.wilson@gmail.com>
2018-05-20 23:29:14 -0700

    Basic bed drawing added.

--- gardenweb/drawing.py
+++ gardenweb/drawing.py
@@ -4,7 +4,7 @@ import svgwrite
 import datetime
 
 pixels_per_foot = 50
-filename = "garden_plan.svg"
+filename = "gardenweb/static/crap.svg"
 image_width = 2000
 image_height = 2000
 
@@ -17,12 +17,31 @@ def feet_to_pixels(feet):
 def pixels_to_feet(pixels):
     return pixels/pixels_per_foot
 
+def calculate_bed_size():
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+    beds = gardenweb.bed.get_beds()
+    for bed in beds:
+        max_x = max(max_x, bed['top_left_x'] + bed['x_length'])
+        max_y = max(max_y, bed['top_left_y'] + bed['y_length'])
+    return (feet_to_pixels(max_x-min_x) + 2*x_padding,
+            feet_to_pixels(max_y-min_y) + 2*y_padding)
 
+def draw_bed(bed, svg_handle, x_padding = 0, y_padding = 0):
+    bed_rect = svg_handle.rect(insert = (x_padding+feet_to_pixels(bed['top_left_x']),
+                                         y_padding+feet_to_pixels(bed['top_left_y'])),
+                               size = (feet_to_pixels(bed['x_length']),
+                                       feet_to_pixels(bed['y_length'])),
+                               class_="bed")
+    svg_handle.add(bed_rect)
 
 def make_bed_svg():
-    dwg = svgwrite.Drawing("gardenweb/static/crap.svg", (2000, 2000))
-    dwg.add(dwg.rect(insert=(10,10),
-                     size=(200,400),
-                     fill="black"))
+    dwg = svgwrite.Drawing(filename = filename,
+                       size = calculate_bed_size()+(x_padding, y_padding))
+    beds = gardenweb.bed.get_beds()
+    for bed in beds:
+        draw_bed(bed, dwg, x_padding = x_padding, y_padding = y_padding)
     dwg.save()
     
